bae9eabd01df55350749ab90fbf184d9294fa9cd
Shun Yin Kam <64185146+EdwinKam@users.noreply.github.com>
2020-06-01 21:48:40 -0700

    changed file's com

--- HelloWorld/src/com/edwin/Main.java
+++ HelloWorld/src/com/edwin/Main.java
@@ -1,8 +1,8 @@
-package com.company;
+package com.edwin;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("hello world");
     }
 }
