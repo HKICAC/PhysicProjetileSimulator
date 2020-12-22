8d4a30f51ec12da321a97160590bb5015d26556d
ansoncht <56498302+ansoncht@users.noreply.github.com>
2020-12-21 23:32:20 -0800

    Updated Layout and changed direction of X

--- script/projectile.js
+++ script/projectile.js
@@ -1,27 +1,38 @@
-var scene, camera, renderer, base, sphere, ground;
+var scene, camera, renderer, base, sphere, ground, ball;
 
-function init(){
+function init() {
     // Create a scene
     scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0xcce0ff );
-    scene.fog = new THREE.Fog( 0xcce0ff, 900, 1000 );
+    scene.background = new THREE.Color(0xcce0ff);
+    scene.fog = new THREE.Fog(0xcce0ff, 900, 1000);
 
     // Create a camera
-    camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    camera.position.set( 0, 0, -5 );
+    camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0, 0, -200);
+
+    // Creates the renderer with size 1280x720
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // Puts the "canvas" into our HTML page.
+    document.body.appendChild(renderer.domElement);
+    create();
+}
+
+function create() {
+
 
     // Create Base
-    const baseTexture = new THREE.TextureLoader().load('./image/wood.jpg');
-    baseTexture.wrapS = baseTexture.wrapT = THREE.MirroredRepeatWrapping;
+    const baseTexture = new THREE.TextureLoader().load('./image/wood.jpg')
     baseTexture.anisotropy = 2;
+    baseTexture.wrapS = baseTexture.wrapT = THREE.MirroredRepeatWrapping;
     baseTexture.repeat.set(2, 2);
     const baseMaterial = new THREE.MeshBasicMaterial( { map: baseTexture } );
     base = new THREE.Mesh(
         new THREE.BoxGeometry(1,1,1), // width, height, depth
         baseMaterial
     );
-    base.position.set(10,-0.1,0);
-    base.scale.set(0.5,3,0);
+    base.position.set(450,-29,0);
+    base.scale.set(10,70,0);
     scene.add(base);
 
     // Create Sphere
@@ -29,16 +40,16 @@ function init(){
         new THREE.SphereGeometry(2, 10, 10), // width, height, depth
         new THREE.MeshBasicMaterial({color:0xff4444})
     );
-    sphere.position.set(10,1.8,0)
-    sphere.scale.set(0.2,0.2,0);
+    sphere.position.set(450,11.5,0);
+    sphere.scale.set(3,3,0);
     scene.add(sphere);
 
-    //const groundTexture = new THREE.TextureLoader().load( './image/grasslight-big.jpg' );
-    //groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
-    //groundTexture.repeat.set( 1000, 1000);
-    //groundTexture.anisotropy = 1000;
+    /*const groundTexture = new THREE.TextureLoader().load( './image/grasslight-big.jpg' );
+    groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+    groundTexture.repeat.set( 1000, 1000);
+    groundTexture.anisotropy = 1000;
 
-    //const groundMaterial = new THREE.MeshLambertMaterial( { map: groundTexture } );
+    const groundMaterial = new THREE.MeshLambertMaterial( { map: groundTexture } );*/
 
     ground = new THREE.Mesh(
         new THREE.PlaneBufferGeometry( 20000, 20000 ),
@@ -52,61 +63,53 @@ function init(){
     // Point the camera to look at 0,0,0
     camera.lookAt(new THREE.Vector3(0,0,0));
 
-    // Creates the renderer with size 1280x720
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    // Puts the "canvas" into our HTML page.
-    document.body.appendChild(renderer.domElement);
-
+    // load scene
     renderer.render(scene, camera);
-    // Begin animation
 }
 
 
 
 //create an velocity component
-function Velocity(x0,y0,vx,vy){ //idk how to change sphere.position.y intial position
-    this.x0=x0;
-    this.y0=y0;
-    this.vx=vx;
-    this.vy=vy;
-    //sphere.position.set(v0,y0,0)
+function Velocity(x0,y0,vx,vy){
+    this.x0 = x0;
+    this.y0 = y0;
+    this.vx = vx;
+    this.vy = vy;
 }
-
-var ball; //this is a velocity object
 
 function maxHeight(){
-    v0 = document.getElementById("v0").value;
-    y0 = document.getElementById("y0").value;
+    var v0 = document.getElementById("v0").value;
+    var y0 = document.getElementById("y0").value;
     var theta = document.getElementById("theta").value * Math.PI/180;
-    vy = Math.sin(theta) * v0; //calculate vy
-    vx = Math.cos(theta)*v0; //calculate vx
-    var t = vy / 9.8
+    var vy = Math.sin(theta) * v0; //calculate vy
+    var vx = Math.cos(theta) * v0; //calculate vx
+    var t = 2 * vy / 9.8;
+    var xf = Number.parseFloat((vx * t)).toPrecision(4);
     var yf = Number.parseFloat((y0 + vy * t + 1 / 2 * 9.8 * t * t)).toPrecision(4);
     document.getElementById("yf").value = +yf;
-    ball=new Velocity(v0,y0,vx,vy); //put values in ball object
+    document.getElementById("xf").value = -xf;
+    ball = new Velocity(v0,y0,vx,vy); // put values in ball object
 }
 
-function display(){
-
-
-    // Rotate our mesh.
-    const groundHeight=-1.2
-    if (sphere.position.y > groundHeight) { //if above ground
+function display() {
+    // Animate
+    const groundHeight = -58;
+    if (sphere.position.y > groundHeight) { // If above ground
         requestAnimationFrame(display); // Tells the browser to smoothly render at 60Hz
         //sphere.rotation.x += 0.01;
-        ball.vy-=9.8/100;
+        ball.vy -= 9.8 / 50;
+        if (ball.vy > 10)
+            ball.vy /= 2.2;
         sphere.rotation.z += 0.02;
-        sphere.position.x -= ball.vx/100;
-        sphere.position.y += ball.vy/100;
+        sphere.position.x += ball.vx;
+        sphere.position.y += ball.vy;
+        // Draw the scene from the perspective of the camera.
+        renderer.render(scene, camera);
     } else {
-        //sphere.position.x = 1;
-        sphere.position.y = groundHeight;
+        cancelAnimationFrame(display);
+        create();
     }
-    // Draw the scene from the perspective of the camera.
-    renderer.render(scene, camera);
 }
-
 // When the page has loaded, run init();
 window.onload = init;
 
